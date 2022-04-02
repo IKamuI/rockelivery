f3808798f2a7c51eb868524e261453326ed7958e
Kamu <joaopaulogabarrao@gmail.com>
2022-04-02 18:18:33 -0300

    create user migration

--- priv/repo/migrations/20220402203352_create_users_table.exs
+++ priv/repo/migrations/20220402203352_create_users_table.exs
@@ -2,6 +2,19 @@ defmodule Rockelivery.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :address, :string
+      add :age, :string
+      add :cep, :string
+      add :cpf, :string
+      add :email, :string
+      add :password_hash, :string
+      add :name, :string
 
+      timestamps()
+    end
+
+    create unique_index(:users, :email)
+    create unique_index(:users, :cpf)
   end
 end
